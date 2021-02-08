f45ce4b0f02d3ab3967901f69ec3a8a8d89328a9
TheyKay <murraykyleb@gmail.com>
2021-02-07 20:05:14 -0700

    initialized some components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import GlobalProvider from './utils/GlobalContext';
+import Admin from './pages/Admin';
+import Checkout from './pages/Checkout';
+import Store from './pages/Store';
+import User from './pages/User';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
