a4253ac86ac1ea2364f27b983a7bc9af7abbf742
TheyKay <murraykyleb@gmail.com>
2021-02-09 20:51:08 -0700

    added fontawesome

--- client/src/components/Dropdown/Dropdown.jsx
+++ client/src/components/Dropdown/Dropdown.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Dropdown.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Dropdown = () => {
   return (
     <>
       <div className="dropdown">
-        <button className="dropbtn">menu</button>
+        <button className="dropbtn"><FontAwesomeIcon icon={faBars} /></button>
         <div className="dropdownContent">
           <a href="/">store</a>
           <a href="/account">account</a>
