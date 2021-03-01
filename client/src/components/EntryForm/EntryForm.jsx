7b19279b9bd79d00ce24e099d9b4693fe007a70b
TheyKay <murraykyleb@gmail.com>
2021-03-01 14:09:29 -0700

    working on entry form

--- client/src/components/EntryForm/EntryForm.jsx
+++ client/src/components/EntryForm/EntryForm.jsx
@@ -9,7 +9,6 @@ const EntryForm = () => {
   };
 
   const showSpecific = (e) => {
-    console.log(e.target.value);
     const category = e.target.value;
     if (document.getElementById("entryCategorySpecific")) {
       document.getElementById("entryCategorySpecific").remove();
@@ -17,10 +16,10 @@ const EntryForm = () => {
     
     const dropdown = document.createElement("select");
     dropdown.setAttribute("defaultValue", "select");
+    dropdown.setAttribute("id", "entryCategorySpecific");
     const defaultOption = document.createElement("option");
     defaultOption.innerText = "select";
     dropdown.appendChild(defaultOption);
-    dropdown.setAttribute("id", "entryCategorySpecific");
     defaultOption.setAttribute("disabled", true);
     
     specifics[category].forEach((item) => {
@@ -32,8 +31,30 @@ const EntryForm = () => {
     document.getElementById("categories").appendChild(dropdown);
   };
 
-  const newOption = () => {
-
+  const deleteEntry = (e) => {
+    // e.preventDefault();
+    console.log("hello");
+  }
+  
+  const newOption = (e) => {
+    e.preventDefault();
+    const parentDiv = document.getElementById("options");
+    const childDiv = document.createElement("div");
+    childDiv.setAttribute("class", "optionDiv");
+    const newLabel = document.createElement("label");
+    newLabel.innerText = "volume:";
+    childDiv.appendChild(newLabel);
+    const newText = document.createElement("input");
+    newText.setAttribute("type", "text");
+    newText.setAttribute("class", "option");
+    childDiv.appendChild(newText);
+    const removeBtn = document.createElement("button");
+    removeBtn.innerText = "-"
+    removeBtn.setAttribute("class", "removeBtn");
+    removeBtn.setAttribute("type", "button");
+    removeBtn.onClick = deleteEntry;
+    childDiv.appendChild(removeBtn);
+    parentDiv.appendChild(childDiv);
   };
 
   return (
@@ -45,10 +66,10 @@ const EntryForm = () => {
         <br />
         {/* ABV whole number */}
         <label htmlFor="entryABVWhole">ABV</label>
-        <input type="number" id="entryABVWhole" min="0" max="95" step="1"></input>
-        {/* ABV decimal */}
+        <input type="text" id="entryABV"></input>
+        {/* ABV decimal
         <label htmlFor="entryABVDecimal">.</label>
-        <input type="number" id="entryABVDecimal" min="0" max="9" step="1"></input>
+        <input type="number" id="entryABVDecimal" min="0" max="9" step="1"></input> */}
         <br />
         <div id="categories">
           {/* General Category */}
@@ -61,93 +82,17 @@ const EntryForm = () => {
           </select>
           {/* specific category appended when general selected */}
         </div>
-        <label hidden={true} htmlFor="entryCategorySpecific">Specific Category</label>
-        <input type="select" id="entryCategorySpecific" hidden={true} list="" autoComplete="on"></input>
-        <datalist id="categoriesWine">
-          <option value="Red"></option>
-          <option value="Pink"></option>
-          <option value="White"></option>
-          <option value="Champagne"></option>
-          <option value="Sparkling"></option>
-          <option value="Dessert"></option>
-          <option value="Specialty"></option>
-          <option value="Sake"></option>
-          <option value="Spritzer"></option>
-          <option value="Variety Pack"></option>
-        </datalist>
-        <datalist id="categoriesBeer">
-          <option value="Ale"></option>
-          <option value="IPA"></option>
-          <option value="Lager"></option>
-          <option value="Specialty/Alternatives"></option>
-          <option value="Sour/Wild Ale"></option>
-          <option value="Cider"></option>
-          <option value="Stout"></option>
-          <option value="Belgian-Style Ale"></option>
-          <option value="Pale Ale"></option>
-          <option value="Wheat Ale"></option>
-          <option value="Pale Lager"></option>
-          <option value="Pilsner"></option>
-          <option value="Hard Seltzer"></option>
-          <option value="Porter"></option>
-          <option value="Golden/Blonde Ale"></option>
-          <option value="Amber/Red Ale"></option>
-          <option value="Seasonal"></option>
-          <option value="Strong Ale"></option>
-          <option value="Variety Pack"></option>
-          <option value="Fruit"></option>
-          <option value="Brown Ale"></option>
-          <option value="Dark Lager"></option>
-          <option value="Malt Liquor"></option>
-          <option value="Kölsch"></option>
-          <option value="Spice/Herb/Vegetable"></option>
-          <option value="Amber/Vienna Lager"></option>
-          <option value="Bock"></option>
-          <option value="Hard Kombucha"></option>
-          <option value="Cream Ale"></option>
-          <option value="Hard Iced Tea"></option>
-          <option value="Hard Soda"></option>
-          <option value="Non-Alcoholic"></option>
-          <option value="Hard Lemonade"></option>
-          <option value="Perry"></option>
-          <option value="Rye"></option>
-          <option value="Smoked/Rauchbier"></option>
-          <option value="Kellerbier/Zwickelbier"></option>
-          <option value="California Common/Steam"></option>
-          <option value="Chili"></option>
-          <option value="Honey"></option>
-        </datalist>
-        <datalist id="categoriesLiquor">
-          <option value="Whiskey"></option>
-          <option value="Vodka"></option>
-          <option value="Liqueur/Cordials/Schnapps"></option>
-          <option value="Tequila"></option>
-          <option value="Rum"></option>
-          <option value="Ready-To-Drink"></option>
-          <option value="Brandy"></option>
-          <option value="Gin"></option>
-          <option value="Mezcal"></option>
-          <option value="Absinthe"></option>
-          <option value="Soju"></option>
-          <option value="Grain Alcohol"></option>
-          <option value="Shochu"></option>
-          <option value="Cachaça"></option>
-          <option value="Non-Alcoholic Spirits"></option>
-        </datalist>
-        <br />
         {/* options - have option to add more entries */}
         {/* volume */}
-        <label htmlFor="entryVolume">Volume</label>
-        <input type="number" id="entryVolume"></input>
+        {/* <label htmlFor="entryVolume">Volume</label>
+        <input disabled={true} type="text" id="entryVolume"></input> */}
+        {/* price */}
+        {/* <label htmlFor="entryCost">$</label>
+        <input disabled={true} type="text" id="entryCost"></input> */}
+        {/* <br /> */}
+        <div id="options"></div>
+        <button onClick={newOption}>add option</button>
         <br />
-        {/* price, dollars */}
-        <label htmlFor="entryCostDollar">$</label>
-        <input type="number" min="0" id="entryCostDollar"></input>
-        {/* price, cents */}
-        <label htmlFor="entryCostCent"></label>
-        <input type="number" min="0" max="99" id="entryCostCent"></input>
-        <br />
-        <button onClick={newOption}>new option</button>
         {/* brand */}
         <label htmlFor="entryBrand">Brand</label>
         <input type="text" id="entryBrand"></input>
@@ -158,7 +103,7 @@ const EntryForm = () => {
         <br />
         {/* description */}
         <label htmlFor="entryDescription">Description</label>
-        <input type="text" id="entryDescription"></input>
+        <textarea rows="3" id="entryDescription"></textarea>
         <br />
         {/* image */}
         <label htmlFor="entryImage">Image</label>
